9f12509b310c508a7d18fd60849bd5e013e64421
GabriPires <gabriel.santos.pes@gmail.com>
2020-04-27 13:51:57 -0300

    Importando e configurando loader de arquivos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import profile from './assets/avatar.jpeg'
+
 function App(){
-  return <h1>Hello Rocketseat</h1>
+  return <img src={profile}/>
 }
 
 export default App;
